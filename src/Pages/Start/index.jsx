21d422ad67cde260d925c0227016ca7499fe78db
Bruna Oliveira <bruna.o.fonseca@hotmail.com>
2022-11-30 15:34:22 -0300

    Estilização da page Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,18 +1,41 @@
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View, StyleSheet } from "react-native";
 
 export default function Start() {
   return(
-    <View>
+    <View style={ styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={ {alignItems : "center"} }>
         <Image 
         source={require("../../assets/icons/logo3.png")}
+        style= {styles.logo}
         />
 
-        <Text>
-        Vamos trnsformar sua vida {"/n"} em jogos, buscando sempre {"/n"} o melhor nível.
+        <Text style={styles.description}>
+        Vamos transformar sua vida {"\n"} em jogos, buscando sempre {"\n"} {" "}o melhor nível.
         </Text>
+
+        </View>
       </ScrollView>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"rgba(21, 21, 21, 0.98)",
+      },
+logo:{
+  width:300,
+  height:60,
+  marginBottom:20,
+  marginTop:60,
+},
+description:{
+  color:"#ffffff",
+  fontSize: 20,
+  textAlign:"center",
+  marginVertical:60,
+},
+    })
