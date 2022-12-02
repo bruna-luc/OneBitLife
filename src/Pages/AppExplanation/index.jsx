c52853299b8a37364829544c0ca3050a250c2545
Bruna Oliveira <bruna.o.fonseca@hotmail.com>
2022-12-02 13:51:55 -0300

    Explanation card finalizado

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,71 +1,65 @@
 import React from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
+import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
-import DefaultButton from "../../Components/Common/DefaultButton"
+export default function AppExplanation() {
+  function handleSetShowHome() {
+    console.log("teste do botão");
+  }
 
+  return (
+    <View style={styles.container}>
+      <ScrollView>
 
-export default function AppExplanation(){
-function handleSetShowHome(){
-console.log("teste do botão");
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.title}>Antes, deixa{"\n"} eu te explicar...</Text>
+          <ExplanationCard/>
+
+          <Text style={styles.descriptionCta}>Pronto(a) para subir de nível na vida?
+          </Text>
+
+          <Text style={styles.description}>
+            Na próxima tela você vai poder escolher {"\n"}seus 4 hábitos de
+            forma individual.
+          </Text>
+          <DefaultButton
+            buttonText={"Continuar"}
+            handlePress={handleSetShowHome}
+            width={250}
+            height={50}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
-  return(
-<View style={styles.container}>
-  <ScrollView>
-    <View style={{alignItems:"center"}}>
-      <Text style={styles.title}>
-        Antes, deixa{"\n"} eu te explicar...
-         
-      </Text>
-      <Text style={styles.descriptionCta}>
-        Pronto(a) para subir de nível na vida?
-      </Text>
-
-      <Text style={styles.description}>
-        Na próxima tela você vai poder escolher {"\n"}seus 4 hábitos de forma individual.
-      </Text>
-    <DefaultButton
-    buttonText={"Continuar"}
-    handlePress={handleSetShowHome}
-    width={250}
-    height={50}
-    />
-
-      </View>
-  </ScrollView>
-</View>
-)
-
-};
-
-const styles= StyleSheet.create({
-  container:{
-flex:1,
-backgroundColor:"rgba(21,21,21,0.98)",
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21,21,21,0.98)",
   },
 
-title:{
-fontSize:30,
-fontWeight:"bold",
-textAlign:"center",
-marginVertical:40,
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color:"#fff",
+    textAlign: "center",
+    marginVertical: 40,
+  },
 
-},
-descriptionCta:{
-color:"#fff",
-fontSize:16,
-fontWeight:"bold",
-marginTop:20,
-marginBottom:10,
-},
-description:{
-  color:"#fff",
-  textAlign:"center",
-  marginBottom:30,
-},
-
-
-
-})
+  descriptionCta: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  description: {
+    color: "#fff",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+});
--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -1,23 +1,22 @@
 // import React from "react";
-import {NavigationContainer} from "@react-navigation/native"
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Start from "../Pages/Start";
+import AppExplanation from "../Pages/AppExplanation";
 
-const Stack =createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 export default function AllPages() {
-  return(
+  return (
     <NavigationContainer>
       <Stack.Navigator
-      screenOptions={{
-        headerShown:false,
-      }}
+        screenOptions={{
+          headerShown: false,
+        }}
       >
-    
-
-<Stack.Screen name="Start" component={Start} />
-
-</Stack.Navigator>
+        <Stack.Screen name="AppExplanation" component={AppExplanation} />
+        <Stack.Screen name="Start" component={Start} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
