493a965652ac41e92a540a901ff77b1bc1353811
Bruna Oliveira <bruna.o.fonseca@hotmail.com>
2022-12-03 10:05:56 -0300

    Edição e Criação dos Hábitos

--- src/Components/Home/EditHabit/index.jsx
+++ src/Components/Home/EditHabit/index.jsx
@@ -1,16 +1,62 @@
 import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from "react-native";
+
+export default function EditHabit({ habit, frequency, habitArea, checkColor}) {
+  function handleEdit() {
+    console.log("Botão de editar clicado");
+  }
+
+  function handleCheck() {
+    console.log(`ckeck do ${habitArea}`);
+  }
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.9}
+      style={styles.button}
+      onPress={handleEdit}
+    >
+      <View style={styles.habitText}>
+        <Text style={styles.habitTitle}>{habit}</Text>
+        <Text style={styles.habitFrequency}>{frequency}</Text>
+      </View>
+
+      <TouchableOpacity
+        style={[styles.check, { borderColor: checkColor }]}
+        onPress={handleCheck}
+      ></TouchableOpacity>
+    </TouchableOpacity>
+  );
+}
+
+styles = StyleSheet.create({
+
+  button: {
+    backgroundColor: "#151515",
+    borderRadius: 5,
+    width: 320,
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  habitTitle: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+
+  habitFrequency: {
+    color: "#fff",
+  },
+  check: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
 
 
 
-
-export default function (){
- 
- 
- 
- 
- 
- 
-  return(
-  
-    )
-};
+});
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,75 +1,114 @@
-import React ,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
-import CreateHabit from "../../Components/Home/CreateHabit"
-// import EditHabit from "../../Components/Home/EditHabit"
+import CreateHabit from "../../Components/Home/CreateHabit";
+import EditHabit from "../../Components/Home/EditHabit";
 
 export default function Home() {
   const navigation = useNavigation();
-  const [mindHabit, setMindHabit]= useState();
-  const [moneyHabit, setMoneyHabit]= useState();
-  const [bodayHabit, setBodayHabit]= useState();
-  const [funHabit, setFunHabit]= useState();
-
+  const [mindHabit, setMindHabit] = useState();
+  const [moneyHabit, setMoneyHabit] = useState();
+  const [bodyHabit, setBodyHabit] = useState();
+  const [funHabit, setFunHabit] = useState();
 
   function hadleNavExplanation() {
-    navigation.navigate("AppExplanation")
+    navigation.navigate("AppExplanation");
   }
 
   return (
-    <View style= {styles.container}>
+    <View style={styles.container}>
       <ScrollView>
         <View style={{ alignItems: "center" }}>
           <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
 
           <LifeStatus />
-          <StatusBar/> 
-          <CreateHabit 
-          habitArea="Mente"
-           borderColor="#90B7F3"/>
-        
-        <Text
-        style={styles.explanationText}
-        onPress={ () => {
-          hadleNavExplanation();
+          <StatusBar />
 
-        }}> Ver explicação novamente 
-        </Text>
-        
-        
+          {mindHabit ? (
+            <EditHabit
+              habit={mindHabit?.habitName}
+              frequency={`${mindHabit.habitTime}-${mindHabit.habitFrequencia}`}
+              habitArea={mindHabit.habitArea}
+              checkColor="#90B7F3"
+            />
+          ) : (
+            <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
+          )}
+
+{moneyHabit ? (
+            <EditHabit
+              habit={moneyHabit?.habitName}
+              frequency={`${moneyHabit.habitTime}-${moneyHabit.habitFrequencia}`}
+              habitArea={moneyHabit.habitArea}
+              checkColor="#85BB65"
+            />
+          ) : (
+            <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
+          )}
+
+{bodyHabit ? (
+            <EditHabit
+              habit={bodyHabit?.habitName}
+              frequency={`${bodyHabit.habitTime}-${bodyHabit.habitFrequencia}`}
+              habitArea={bodyHabit.habitArea}
+              checkColor="#FF0044"
+            />
+          ) : (
+            <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
+          )}
+
+{funHabit ? (
+            <EditHabit
+              habit={funHabit?.habitName}
+              frequency={`${funHabit.habitTime}-${funHabit.habitFrequencia}`}
+              habitArea={funHabit.habitArea}
+              checkColor="#FE7F23"
+            />
+          ) : (
+            <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
+          )}
+
+
+
+
+
+
         </View>
+        <Text
+          style={styles.explanationText}
+          onPress={() => {
+            hadleNavExplanation();
+          }}
+        >
+          {" "}
+          Ver explicação novamente
+        </Text>
       </ScrollView>
     </View>
   );
-};
-
+}
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     backgroundColor: "rgba(21, 21, 21, 0.98)",
   },
   dailyChecks: {
     color: "#fff",
     fontWeight: "bold",
-    textAlign:"center",
-    fontSize:18,
-    marginTop:40,
+    textAlign: "center",
+    fontSize: 18,
+    marginTop: 40,
   },
-explanationText:{
-color:"#fff",
-fontWeight: "bold",
-textAlign:"center",
-fontSize:16,
-paddingTop:15,
-paddingBottom:25,
-},
-
-
-
-
-
-})
+  explanationText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16,
+    paddingTop: 15,
+    paddingBottom: 25,
+  },
+});
